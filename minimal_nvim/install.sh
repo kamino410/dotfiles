c158c90699f306022763196adcfc2e62db793cf0
kamino410 <kamino.dev@gmail.com>
2019-01-24 00:32:49 +0900

    change indent size of cpp 2 -> 4

--- minimal_nvim/install.sh
+++ minimal_nvim/install.sh
@@ -10,10 +10,13 @@ python3 -m pip install pynvim neovim
 mkdir ~/.config
 mkdir ~/.config/dein
 mkdir ~/.config/nvim
+mkdir ~/.config/nvim/ftplugin
 
 mkdir ~/github
 cd ~/github
 git clone https://github.com/kamino410/dotfiles.git
 cp ./dotfiles/minimal_nvim/init.vim ~/.config/nvim
 cp ./dotfiles/minimal_nvim/plugins.toml ~/.config/dein
+cp ./dotfiles/minimal_nvim/cpp.vim ~/.config/nvim/ftplugin
 cd ~
+
