1bf4531d867c11020410ea908c0282b338eabba2
kamino410 <kamino.dev@gmail.com>
2019-08-09 15:45:05 +0900

    fixed some points

--- minimal_nvim/install.sh
+++ minimal_nvim/install.sh
@@ -1,17 +1,13 @@
 apt update
-
 apt install -y software-properties-common
 add-apt-repository -y ppa:neovim-ppa/unstable
 apt update
-apt install -y neovim python3-pip git clang clang-tools curl clang-format
-
-python3 -m pip install pynvim neovim neovim-remote
-
+apt install -y neovim python3-pip git clang clang-tools curl clang-format peco
+python3 -m pip install pynvim neovim neovim-remote autopep8
 mkdir ~/.config
 mkdir ~/.config/dein
 mkdir ~/.config/nvim
 mkdir ~/.config/nvim/ftplugin
-
 mkdir ~/github
 cd ~/github
 git clone https://github.com/kamino410/dotfiles.git
@@ -19,4 +15,13 @@ cp ./dotfiles/minimal_nvim/init.vim ~/.config/nvim
 cp ./dotfiles/minimal_nvim/plugins.toml ~/.config/dein
 cp ./dotfiles/minimal_nvim/cpp.vim ~/.config/nvim/ftplugin
 cd ~
+echo EOF
+alias nvrr='nvr --remote-tab'
+peco-select-history() {
+    declare l=$(HISTTIMEFORMAT= history | sort -k1,1nr | perl -ne 'BEGIN { my @lines = (); } s/^\s*\d+\s*//; $in=$_; if (!(grep {$in eq $_} @lines)) { push(@lines, $in); print $in; }' | peco --query "$READLINE_LINE")
+    READLINE_LINE="$l"
+    READLINE_POINT=${#l}
+}
+bind -x '"\C-r": peco-select-history'
+EOF >> ~/.bashrc
 
