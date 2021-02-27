3765c9cbcfdb6acaa9e7a810e29ce55d40fead47
kamino410 <kamino.dev@gmail.com>
2021-02-27 23:53:07 +0900

    update config.fish

--- .config/config.fish
+++ .config/config.fish
@@ -1,5 +1,7 @@
 export LC_ALL=en_US.UTF-8
 
+set -u fish_term24bit
+
 function peco_select_history
       history|peco|read foo
       if [ $foo ]
@@ -11,4 +13,8 @@ end
 bind \cr 'peco_select_history (commandline -b)'
 
 alias nvrr 'nvr --remote-tab'
+
+set PATH $HOME/.cargo/bin /opt/nvim-osx64/bin /usr/local/bin $PATH
+
+source ~/.iterm2_shell_integration.fish
 
